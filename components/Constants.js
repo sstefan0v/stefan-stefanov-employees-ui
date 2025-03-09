17fe8b5493dc370c29456ec4087a572f82d5e5b8
Stefan Stefanov <elken@abv.bg>
2025-03-09 10:14:12 +0200

    #add more config files

---
+++ components/Constants.js
@@ -0,0 +1,8 @@
+"use client";
+const BASE_URL ='http://localhost:8068';
+
+const Constants = {
+  EMPLOYEES_URL:  BASE_URL + '/employees'
+}
+
+export default Constants;
--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { unoptimized: true }, 
-  experimental: {
-    appDir: true
-  },
+  images: { unoptimized: true },
   output: "export"
 }
 
